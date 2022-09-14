c98adab17babc76dde13e874e23a4ee0bc07d2f0
Sinus2x <hatin.max2002@gmail.com>
2022-09-15 00:04:19 +0300

    created structure of the class

---
+++ hw2/main.py
@@ -0,0 +1,31 @@
+
+
+class CountVectorizer:
+
+    def __init__(self, corpus: [[str]]):
+        self.__features = []
+
+    def fit_transform(self):
+        pass
+
+    @property
+    def get_feature_names(self):
+        return self.__features
+
+
+if __name__ == '__main__':
+    corpus = [
+        'Crock Pot Pasta Never boil pasta again',
+        'Pasta Pomodoro Fresh ingredients Parmesan to taste'
+    ]
+    vectorizer = CountVectorizer(corpus)
+    # count_matrix = vectorizer.fit_transform(corpus)
+
+    print(vectorizer.get_feature_names)
+    # Out: ['crock', 'pot', 'pasta', 'never', 'boil', 'again', 'pomodoro',
+    #       'fresh', 'ingredients', 'parmesan', 'to', 'taste']
+
+    # print(count_matrix)
+    # Out: [[1, 1, 2, 1, 1, 1, 0, 0, 0, 0, 0, 0],
+    #       [0, 0, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1]]
+
