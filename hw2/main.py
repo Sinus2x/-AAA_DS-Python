20e3237f60607185b6ddac0791b247f3fa51cf4d
Sinus2x <hatin.max2002@gmail.com>
2022-09-15 13:01:29 +0300

    naive solution

--- hw2/main.py
+++ hw2/main.py
@@ -1,17 +1,28 @@
 from itertools import chain
+from collections import Counter
 
 
 class CountVectorizer:
 
-    def __init__(self, corpus: [[str]], lowercase=True):
-        words = [sent.lower().split() if lowercase else sent.split() for sent in corpus]
-        self.__features = list(set(chain.from_iterable(words)))
+    def __init__(self, corpus: [str], lowercase=True):
+        sentences = [sent.lower().split() if lowercase else sent.split() for sent in corpus]
+        self.__features = list(set(chain.from_iterable(sentences)))
+        self.__n_features = len(self.__features)
 
-    def fit_transform(self):
-        pass
+    def fit_transform(self, corpus: [str], lowercase=True) -> [[int]]:
+        tdm = []
+        counters = [dict(zip(self.__features, [0] * self.__n_features)) for _ in corpus]
+        counters = zip(counters, corpus)
+        for c, sent in counters:
+            tokens = sent.lower().split() if lowercase else sent.split()
+            for w in tokens:
+                if w in self.__features:
+                    c[w] += 1
+            tdm.append(list(c.items()))
+        return tdm
 
     @property
-    def get_feature_names(self):
+    def get_feature_names(self) -> [str]:
         return self.__features
 
 
@@ -20,14 +31,14 @@ if __name__ == '__main__':
         'Crock Pot Pasta Never boil pasta again',
         'Pasta Pomodoro Fresh ingredients Parmesan to taste'
     ]
+
     vectorizer = CountVectorizer(corpus)
-    # count_matrix = vectorizer.fit_transform(corpus)
+    count_matrix = vectorizer.fit_transform(corpus)
 
     print(vectorizer.get_feature_names)
     # Out: ['crock', 'pot', 'pasta', 'never', 'boil', 'again', 'pomodoro',
     #       'fresh', 'ingredients', 'parmesan', 'to', 'taste']
 
-    # print(count_matrix)
+    print(count_matrix)
     # Out: [[1, 1, 2, 1, 1, 1, 0, 0, 0, 0, 0, 0],
     #       [0, 0, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1]]
-
