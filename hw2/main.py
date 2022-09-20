1c20c97bc8ed5913b9640ee13acae3cbf7a8985c
Sinus2x <hatin.max2002@gmail.com>
2022-09-20 12:49:08 +0300

    splitted class-code and run-code into 2 files

--- hw2/main.py
+++ hw2/main.py
@@ -1,31 +1,6 @@
-from itertools import chain
-
-
-class CountVectorizer:
-
-    def __init__(self, corpus: [str], lowercase=True):
-        sentences = [sent.lower().split() if lowercase else sent.split() for sent in corpus]
-        self.__features = list(set(chain.from_iterable(sentences)))
-        self.__n_features = len(self.__features)
-
-    def fit_transform(self, corpus: [str], lowercase=True) -> [[int]]:
-        tdm = []
-        counters = [dict.fromkeys(self.__features, 0) for _ in corpus]
-        counters = zip(counters, corpus)
-        for c, sent in counters:
-            tokens = sent.lower().split() if lowercase else sent.split()
-            for w in tokens:
-                if w in self.__features:
-                    c[w] += 1
-            tdm.append(list(c.values()))
-        return tdm
-
-    @property
-    def get_feature_names(self) -> [str]:
-        return self.__features
-
-
 if __name__ == '__main__':
+    from vectorizer import CountVectorizer
+
     corpus = [
         'Crock Pot Pasta Never boil pasta again',
         'Pasta Pomodoro Fresh ingredients Parmesan to taste'
