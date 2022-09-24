a1676ca06c6df95975d2a4643ae02b9983aff6d7
Sinus2x <hatin.max2002@gmail.com>
2022-09-24 11:44:47 +0300

    solution

--- hw3/main.py
+++ hw3/main.py
@@ -13,12 +13,15 @@ class ObjFromJSON:
 
 
 class ColorizeMixin:
+    repr_color_code = 33
+
     def __repr__(self):
-        print(1)
+        return f'\033[1;{ColorizeMixin.repr_color_code};40m '
 
 
 class Advert(ColorizeMixin, ObjFromJSON):
     def __init__(self, data: dict[str, object]):
+        # initialization from dict
         # check if 'title' in fields
         if 'title' not in data.keys():
             raise KeyError('Title is a required field')
@@ -32,16 +35,31 @@ class Advert(ColorizeMixin, ObjFromJSON):
         super().__init__(data)
 
     def __repr__(self):
-        return f'{self.title} | {self.price} ₽'
+        return super().__repr__() + f'{self.title} | {self.price} ₽'
 
 
 if __name__ == '__main__':
-    print(Advert(json.loads("""{
-    "title": "python", "price": 0,
-    "class": "d",
-    "location": {
-    "address": "город Москва, Лесная, 7",
-    "metro_stations": ["Белорусская"]
-    }
-    }""")))
-    print(Advert.mro())
+    ad1_str = """{
+"title": "python", "class": "lesson",
+"location": {
+"address": "город Москва, Лесная, 7",
+"metro_stations": ["Белорусская"]
+}
+}"""
+    ad1 = json.loads(ad1_str)
+    print(Advert(ad1))
+    print(Advert(ad1).__dict__)
+
+    ad2_str = """{
+"title": "Вельш-корги",
+"price": 1000,
+"class": "dogs",
+"location": {
+"address": "сельское поселение Ельдигинское, поселок санатория
+Тишково, 25"
+}
+}"""
+
+    ad2 = json.loads(ad2_str, strict=False)
+    print(Advert(ad2))
+
